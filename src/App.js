c08cd12824ad58e40bf3c43f7fd9ee090817d0ad
ebenofere <eofere@gmail.com>
2022-07-26 00:09:29 +0100

    UI update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import React from 'react';
-import { Box, Text, Link, VStack, Code, Grid } from '@chakra-ui/react';
+import {
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  GridItem,
+} from '@chakra-ui/react';
 import { bookList } from './store';
 import Books from './Books';
 
 function App() {
   return (
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <VStack spacing={8}>
-          <Text>Welcome to Canada</Text>
-          <Books items={bookList} />
-        </VStack>
-      </Grid>
-    </Box>
+    <Grid
+      templateAreas={`"header header" "nav main" "nav footer"`}
+      gridTemplateRows={'50px 1fr 30px'}
+      gridTemplateColumns={'150px 1fr'}
+      h="500px"
+      gap="1"
+      color="blackAlpha.700"
+      fontWeight="bold"
+    >
+      <GridItem pl="2" bg="orange.300" area={'header'}>
+        Header
+      </GridItem>
+      <GridItem pl="2" bg="pink.300" area={'nav'}>
+        Nav
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={'main'}>
+        <Books items={bookList} />
+      </GridItem>
+      <GridItem pl="2" bg="blue.300" area={'footer'}>
+        Footer
+      </GridItem>
+    </Grid>
   );
 }
 
 export default App;
+
+// <Box textAlign="center" fontSize="xl">
+//       <Grid minH="100vh" p={3}>
+//         <VStack spacing={8}>
+//           <Text>Welcome to Canada</Text>
+//           <Books items={bookList} />
+//         </VStack>
+//       </Grid>
+//     </Box>
--- src/BookItem.js
+++ src/BookItem.js
@@ -1,18 +1,62 @@
 import React from 'react';
-import { Box, Text, Link, VStack, Code, Grid, Stack } from '@chakra-ui/react';
+import {
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  Stack,
+  Image,
+} from '@chakra-ui/react';
 
 const BookItem = ({ items }) => {
   console.log(items, 'items');
 
   return (
     <Stack>
-      {items.map(item => {
-        <Text key={item.id}>Book Name: {item.itemName}</Text>;
-        <Text key={item.id}>Author: {item.itemAuthor}</Text>;
-        <Text key={item.id}>Year Published: {item.itemPublishedYear}</Text>;
-      })}
+      {items.map(item => (
+        <Box w="100%" p={4}>
+          <Box display="flex">
+            <Box>
+              <Image
+                src={item.itemImage}
+                alt={item.itemName}
+                boxSize="320px"
+                objectFit="cover"
+                borderRadius="md"
+              />
+              <Box
+                as="button"
+                borderRadius="md"
+                bg="teal.500"
+                color="white"
+                px={4}
+                h={8}
+              >
+                Add Item
+              </Box>
+            </Box>
+            <Box>
+              <Box>{item.itemName}</Box>
+              <Box>{item.itemAuthor}</Box>
+              <Box display="flex">
+                <Box>{item.itemPublishedYear}</Box>
+                <Box>Pages: {item.itemTotalPages}</Box>
+                <Box>{item.itemPublisher}</Box>
+              </Box>
+              <Box>ISBN: {item.ISBN}</Box>
+              <Box>{item.itemDescription}</Box>
+            </Box>
+          </Box>
+        </Box>
+      ))}
     </Stack>
   );
 };
 
 export default BookItem;
+
+// <Text fontSize="md" key={item.id}>
+// Book Name: {item.itemName}
+// </Text>
