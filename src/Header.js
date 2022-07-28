15ee571075b52c4c7aab78a06c664adbb1f2860f
ebenofere <eofere@gmail.com>
2022-07-28 15:54:32 +0100

    update UI

--- src/Header.js
+++ src/Header.js
@@ -7,38 +7,47 @@ import {
   ListIcon,
   UnorderedList,
   Flex,
+  Spacer,
+  Box,
 } from '@chakra-ui/react';
 import { MdEdit, MdViewList, MdBookmark, MdDelete } from 'react-icons/md';
+import { CloseIcon } from '@chakra-ui/icons';
 
 export const Header = props => {
   return (
-    <List p={3} ml={10}>
-      <Flex gap={10}>
-        <ListItem color="black">
-          <Flex minWidth="max-content" alignItems="center">
-            <ListIcon as={MdEdit} />
-            Edit
-          </Flex>
-        </ListItem>
-        <ListItem color="black">
-          <Flex minWidth="max-content" alignItems="center">
-            <ListIcon as={MdViewList} />
-            Details
-          </Flex>
-        </ListItem>
-        <ListItem color="black">
-          <Flex minWidth="max-content" alignItems="center">
-            <ListIcon as={MdBookmark} />
-            Flag
-          </Flex>
-        </ListItem>
-        <ListItem color="red.500">
-          <Flex minWidth="max-content" alignItems="center">
-            <ListIcon as={MdDelete} color="red.500" />
-            Delete
-          </Flex>
-        </ListItem>
-      </Flex>
-    </List>
+    <Flex>
+      <List p={3} ml={10}>
+        <Flex gap={10}>
+          <ListItem color="black">
+            <Flex minWidth="max-content" alignItems="center">
+              <ListIcon as={MdEdit} />
+              Edit
+            </Flex>
+          </ListItem>
+          <ListItem color="black">
+            <Flex minWidth="max-content" alignItems="center">
+              <ListIcon as={MdViewList} />
+              Details
+            </Flex>
+          </ListItem>
+          <ListItem color="black">
+            <Flex minWidth="max-content" alignItems="center">
+              <ListIcon as={MdBookmark} />
+              Flag
+            </Flex>
+          </ListItem>
+          <ListItem color="red.500">
+            <Flex minWidth="max-content" alignItems="center">
+              <ListIcon as={MdDelete} color="red.500" />
+              Delete
+            </Flex>
+          </ListItem>
+        </Flex>
+      </List>
+      <Spacer />
+      <Box p={3} mr={2}>
+        <CloseIcon w={4} h={4} color="black" />
+      </Box>
+    </Flex>
   );
 };
